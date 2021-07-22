37fb53a53a925f3721cbd39a14b41859e51eb58d
sk044 <shubham_1901mm32@iitp.ac.in>
2021-07-22 06:23:41 +0530

    cors

--- server.js
+++ server.js
@@ -5,21 +5,16 @@ const cors = require('cors');
 
 app.use(express.json());
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
-// app.use(function(req, res, next) {
-//     res.setHeader("Access-Control-Allow-Origin", '*');
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     res.setHeader("Access-Control-Max-Age", "1800");
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Max-Age", "1800");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 dotenv.config( { path : 'config.env'} );
 
