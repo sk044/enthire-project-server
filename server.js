7323f8b8ec64052d540f3a893b4bf76bed79e9bd
sk044 <shubham_1901mm32@iitp.ac.in>
2021-07-22 06:44:14 +0530

    cors mc

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
 const cors = require('cors');
 
+// connect mongodb database
+const app = express();
 app.use(express.json());
-
 app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Max-Age", "1800");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
 dotenv.config( { path : 'config.env'} );
-
-// connect mongodb database
+const PORT = process.env.PORT || 8080;
 require('./database/database')();
 
 // calling routes
 app.use('/', require('./routes/router'));
 
-app.listen(5000, () => console.log(`Server is stated on http://localhost:5000`));
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
