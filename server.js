52635e6bc453f8c40056fa96aba9b4fe04b7667d
sk044 <shubham_1901mm32@iitp.ac.in>
2021-07-22 06:17:45 +0530

    cors

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const cors = require('cors');
 
 app.use(express.json());
 
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // app.use(function(req, res, next) {
 //     res.setHeader("Access-Control-Allow-Origin", '*');
