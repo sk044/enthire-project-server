9a23bae53877f098e2d29df7108eaf8b79f69b58
sk044 <shubham_1901mm32@iitp.ac.in>
2021-07-22 07:26:45 +0530

    cors mc

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ const app = express();
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,Authorization,application/json,multipart/form-data');
-    next();
+    res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      // intercept OPTIONS method
+      if ('OPTIONS' == req.method) {
+          res.status(200).send();
+      } else {
+          next();
+      }
 });
 
 app.use(express.json());
